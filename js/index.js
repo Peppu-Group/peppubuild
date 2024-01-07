7040713a2be1b0fc90f9eaec6f4f5daf369f0b9c
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2024-01-07 16:34:02 +0100

    imitial commit of peppubuild

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+    import axios from 'axios';
+    
+    export default {
+        /*Insert Imports Here*/ 
+
+        setup() {
+          return { 
+            /*Insert Data Here*/
+          }
+        },
+        methods: {
+            /*Insert Methods Here*/
+        },
+        async mounted() {
+            /*Insert Mounted Here*/
+        }
+    }
+    
